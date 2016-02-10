287f0e852fb442c12b1c377fdb8b103f8a2e4c77
omi_yusuke <omi_yusuke@liginc.co.jp>
2016-02-10 22:21:29 +0900

    add HTML task

--- gulp/step1.js
+++ gulp/step1.js
@@ -1,11 +1,29 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass')
+var sass = require('gulp-sass');
 var notify = require('gulp-notify');
 var plumber = require('gulp-plumber');
+var htmlhint = require("gulp-htmlhint");
 
-gulp.task('step1', function() {
+gulp.task('sass', function() {
     gulp.src('app/src/sass/*.scss')
+        .pipe(plumber({
+            errorHandler: notify.onError("Error: <%= error.message %>")
+        }))
         .pipe(sass())
         .pipe(notify('scss was compiled!'))
         .pipe(gulp.dest('app/product/css'));
 });
+
+gulp.task('html', function() {
+    return gulp.src('app/src/**/*.html')
+        .pipe(htmlhint())
+        .pipe(htmlhint.reporter())
+        .pipe(gulp.dest('app/product'));
+});
+
+gulp.task('watch', function(){
+    gulp.watch('./app/src/sass/*.scss', ['sass']);
+    gulp.watch('./app/src/**/*.html', ['html']);
+});
+
+gulp.task('default', ['sass']);
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,1 @@
-require('./gulp/clean');
-require('./gulp/sass');
-require('./gulp/script');
 require('./gulp/step1');
