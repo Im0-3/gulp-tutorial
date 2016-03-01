26104324e8139961194ac7b50741410a76fd340c
omi_yusuke <omi_yusuke@liginc.co.jp>
2016-03-01 16:00:38 +0900

    gulp #3 demo

---
+++ 3-2/project/gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var htmlhint = require('gulp-htmlhint');
+
+gulp.task('sass', function() {
+    gulp.src('app/src/sass/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('app/product/css/'));
+});
+
+gulp.task('html', function(){
+    gulp.src('app/src/**/*.html')
+        .pipe(htmlhint())
+        .pipe(htmlhint.reporter())
+        .pipe(gulp.dest('app/product/'));
+});
+
+gulp.task('default', function() {
+    gulp.watch('app/src/sass/*.scss',['sass']);
+    gulp.watch('app/src/**/*.html',['html']);
+});
