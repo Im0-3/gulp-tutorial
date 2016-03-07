a71f8e620a1fbea474dbd0961b117f36a2728792
omiyusuke <omi_yusuke@liginc.co.jp>
2016-03-07 23:56:25 +0900

    4-1, 4-2を追加

---
+++ 4-2/project/gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var htmlhint = require('gulp-htmlhint');
+var browserSync = require('browser-sync').create();
+var notify = require('gulp-notify');
+
+gulp.task('sass', function() {
+    gulp.src('app/src/sass/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('app/product/css/'))
+        .pipe(browserSync.stream())
+        .pipe(notify('scssをcssにコンパイルしました'));
+});
+
+gulp.task('html', function(){
+    gulp.src('app/src/**/*.html')
+        .pipe(htmlhint())
+        .pipe(htmlhint.reporter())
+        .pipe(gulp.dest('app/product/'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: "./app/product"
+    });
+});
+
+gulp.task('default', function() {
+    browserSync.init({
+        server: "./app/product"
+    });
+
+    gulp.watch('app/src/sass/*.scss',['sass']);
+    gulp.watch('app/src/**/*.html',['html']);
+});
